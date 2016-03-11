0bf2f3a4660cc42f7a33db1625491be0fa7782d7
surdu <postal2600@gmail.com>
2016-03-12 00:22:55 +0200

    Added minifing for deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,10 @@ module.exports = function(grunt) {
 	"use strict";
 
 	var path = require('path');
+
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-webpack');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.initConfig({
 		watch: {
@@ -19,9 +21,9 @@ module.exports = function(grunt) {
 		},
 
 		webpack: {
-			static: {
+			dev: {
 				entry: './static/js/app.js',
-				output: { path: path.join(__dirname, "/dist"), filename: 'bundle.js' },
+				output: { path: path.join(__dirname, "/dist"), filename: 'bundle.min.js' },
 				watch: true,
 				devtool: "eval-cheap-module-source-map",
 				module: {
@@ -36,9 +38,34 @@ module.exports = function(grunt) {
 						}
 					]
 				}
+			},
+			deploy: {
+				entry: './static/js/app.js',
+				output: { path: path.join(__dirname, "/dist"), filename: 'bundle.js' },
+				module: {
+					loaders: [
+						{
+							test: /.js?$/,
+							loader: 'babel-loader',
+							exclude: /node_modules/,
+							query: {
+								presets: ['es2015', 'react']
+							}
+						}
+					]
+				}
+			}
+		},
+
+		uglify: {
+			static: {
+				files: {
+					'dist/bundle.min.js': ['dist/bundle.js']
+				}
 			}
 		}
 	});
 
-	grunt.registerTask("start", ["webpack", "watch"]);
+	grunt.registerTask("build", ["webpack:deploy", "uglify"]);
+	grunt.registerTask("watch", ["webpack:dev", "watch"]);
 };
